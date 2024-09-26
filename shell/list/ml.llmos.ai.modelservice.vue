1596b0f265e513d9b7a5156e716de4dc182b6911
Guangbo Chen <steven.guangbo.chen@gmail.com>
2024-09-26 23:42:42 +0800

    Small enhancement of ms & notebook workload

--- shell/list/ml.llmos.ai.modelservice.vue
+++ shell/list/ml.llmos.ai.modelservice.vue
@@ -64,7 +64,7 @@ export default {
       const STATUS = {
         name:  'status',
         label: 'Status',
-        value: 'msStatus'
+        value: 'stateStatus'
       };
 
       const headers = [
