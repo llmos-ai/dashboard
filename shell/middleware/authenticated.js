e42ca0bd4c85852608ffa1464fcfacb23342f0b7
Guangbo Chen <steven.guangbo.chen@gmail.com>
2024-11-23 16:38:30 +0800

    refactor: update steve schemaDefinitions endpoint rc-2.9-e251654

--- shell/middleware/authenticated.js
+++ shell/middleware/authenticated.js
@@ -1,4 +1,3 @@
-import { NAME as LLMOS } from '@shell/config/product/llmos';
 import { TIMED_OUT, UPGRADED, _FLAGGED, _UNFLAG } from '@shell/config/query-params';
 import { MANAGEMENT } from '@shell/config/types';
 import { _ALL_IF_AUTHED } from '@shell/plugins/dashboard-store/actions';
@@ -8,120 +7,10 @@ import { get } from '@shell/utils/object';
 import { setFavIcon, haveSetFavIcon } from '@shell/utils/favicon';
 import dynamicPluginLoader from '@shell/pkg/dynamic-plugin-loader';
 import { BACK_TO } from '@shell/config/local-storage';
-import { canViewResource } from '@shell/utils/auth';
-
-const getPackageFromRoute = (route) => {
-  if (!route?.meta) {
-    return;
-  }
-  // Sometimes meta is an array... sometimes not
-  const arraySafe = Array.isArray(route.meta) ? route.meta : [route.meta];
-
-  return arraySafe.find((m) => !!m.pkg)?.pkg;
-};
-
-const getResourceFromRoute = (to) => {
-  return to.params?.resource;
-};
+import { findMe, invalidResource, setProduct } from '@shell/utils/auth';
+import { getClusterFromRoute, getProductFromRoute, getPackageFromRoute } from '@shell/utils/router';
 
 let beforeEachSetup = false;
-
-function findMeta(route, key) {
-  if (route?.meta) {
-    const meta = Array.isArray(route.meta) ? route.meta : [route.meta];
-
-    for (let i = 0; i < meta.length; i++) {
-      if (meta[i][key]) {
-        return meta[i][key];
-      }
-    }
-  }
-
-  return undefined;
-}
-
-export function getClusterFromRoute(to) {
-  let cluster = to.params?.cluster;
-
-  if (!cluster) {
-    cluster = findMeta(to, 'cluster');
-  }
-
-  return cluster;
-}
-
-export function getProductFromRoute(to) {
-  let product = to.params?.product;
-
-  if ( !product ) {
-    const match = to.name?.match(/^c-cluster-([^-]+)/);
-
-    if ( match ) {
-      product = match[1];
-    }
-  }
-
-  // If still no product, see if the route indicates the product via route metadata
-  if (!product) {
-    product = findMeta(to, 'product');
-  }
-
-  return product;
-}
-
-function setProduct(store, to, redirect) {
-  let product = getProductFromRoute(to);
-
-  // since all products are hardcoded as routes (ex: c-local-explorer), if we match the wildcard route it means that the product does not exist
-  if ((product && (!to.matched.length || (to.matched.length && to.matched[0].path === '/c/:cluster/:product'))) ||
-  // if the product grabbed from the route is not registered, then we don't have it!
-  (product && !store.getters['type-map/isProductRegistered'](product))) {
-    store.dispatch('loadingError', new Error(store.getters['i18n/t']('nav.failWhale.productNotFound', { productNotFound: product }, true)));
-
-    return () => redirect(302, '/fail-whale');
-  }
-
-  if ( !product ) {
-    product = LLMOS;
-  }
-
-  const oldProduct = store.getters['productId'];
-
-  if ( product !== oldProduct ) {
-    store.commit('setProduct', product);
-  }
-
-  return false;
-}
-
-/**
- * Check that the resource is valid, if not redirect to fail whale
- *
- * This requires that
- * - product is set
- * - product's store is set and setup (so we can check schema's within it)
- * - product's store has the schemaFor getter (extension stores might not have it)
- * - there's a resource associated with route (meta or param)
- */
-function invalidResource(store, to, redirect) {
-  const product = store.getters['currentProduct'];
-  const resource = getResourceFromRoute(to);
-
-  // In order to check a resource is valid we need these
-  if (!product || !resource) {
-    return false;
-  }
-
-  if (canViewResource(store, resource)) {
-    return false;
-  }
-
-  // Unknown resource, redirect to fail whale
-
-  store.dispatch('loadingError', new Error(store.getters['i18n/t']('nav.failWhale.resourceNotFound', { resource }, true)));
-
-  return () => redirect(302, '/fail-whale');
-}
 
 export default async function({
   route, app, store, redirect, $cookies, req, isDev, from, $plugin, next
@@ -331,8 +220,4 @@ export default async function({
       return redirect(302, '/fail-whale');
     }
   }
-}
-
-async function findMe(store) {
-  return store.getters['auth/user'];
 }
--- shell/utils/auth.js
+++ shell/utils/auth.js
@@ -1,4 +1,7 @@
 import { allHash } from '@shell/utils/promise';
+import { getProductFromRoute, getResourceFromRoute } from '@shell/utils/router';
+import { NAME as LLMOS } from '@shell/config/product/llmos';
+import { TIMED_OUT } from '@shell/config/query-params';
 
 export const checkSchemasForFindAllHash = (types, store) => {
   const hash = {};
@@ -36,3 +39,123 @@ export const canViewResource = (store, resource) => {
 
   return !!validResource;
 };
+
+// ************************************************************
+//
+// BELOW ARE METHODS THAT ARE A PART OF THE AUTHENTICATED MIDDLEWARE REMOVAL. THIS IS A TEMPORARY HOME FOR THESE UTILS AND SHOULD BE REWRITTEN, MOVED OR DELETED.
+//
+// ************************************************************
+
+/**
+ * Attempt to set the product in our datastore if the route matches a known product. Otherwise show an error page instead.
+ */
+export function setProduct(store, to, redirect) {
+  let product = getProductFromRoute(to);
+
+  // since all products are hardcoded as routes (ex: c-local-explorer), if we match the wildcard route it means that the product does not exist
+  if ((product && (!to.matched.length || (to.matched.length && to.matched[0].path === '/c/:cluster/:product'))) ||
+    // if the product grabbed from the route is not registered, then we don't have it!
+    (product && !store.getters['type-map/isProductRegistered'](product))) {
+    store.dispatch('loadingError', new Error(store.getters['i18n/t']('nav.failWhale.productNotFound', { productNotFound: product }, true)));
+
+    return () => redirect(302, '/fail-whale');
+  }
+
+  if ( !product ) {
+    product = LLMOS;
+  }
+
+  const oldProduct = store.getters['productId'];
+
+  if ( product !== oldProduct ) {
+    store.commit('setProduct', product);
+  }
+
+  return false;
+}
+
+/**
+ * Check that the resource is valid, if not redirect to fail whale
+ *
+ * This requires that
+ * - product is set
+ * - product's store is set and setup (so we can check schema's within it)
+ * - product's store has the schemaFor getter (extension stores might not have it)
+ * - there's a resource associated with route (meta or param)
+ */
+export function invalidResource(store, to, redirect) {
+  const product = store.getters['currentProduct'];
+  const resource = getResourceFromRoute(to);
+
+  // In order to check a resource is valid we need these
+  if (!product || !resource) {
+    return false;
+  }
+
+  if (canViewResource(store, resource)) {
+    return false;
+  }
+
+  // Unknown resource, redirect to fail whale
+  const error = new Error(store.getters['i18n/t']('nav.failWhale.resourceNotFound', { resource }, true));
+
+  store.dispatch('loadingError', error);
+
+  return () => redirect(302, '/fail-whale');
+}
+
+/**
+ * Attempt to load the current user's principal
+ */
+export async function findMe(store) {
+  return store.getters['auth/user'];
+}
+
+/**
+ * Attempt to login with default credentials. Note: I think that this may actually be outdated since we don't use these default credentials anymore on setup.
+ */
+export async function tryInitialSetup(store, password = 'admin') {
+  try {
+    const res = await store.dispatch('auth/login', {
+      provider: 'local',
+      body:     {
+        username: 'admin',
+        password
+      },
+    });
+
+    return res._status === 200;
+  } catch (e) {
+    console.error('Error trying initial setup', e); // eslint-disable-line no-console
+
+    return false;
+  }
+}
+
+/**
+ * Record in our state management that we're indeed logged in
+ */
+export function isLoggedIn(store, me) {
+  store.commit('auth/hasAuth', true);
+  store.commit('auth/loggedInAs', me.id);
+}
+
+/**
+ * Record in our state management that we're not logged in and then redirect to the login page
+ */
+export function notLoggedIn(store, redirect, route) {
+  store.commit('auth/hasAuth', true);
+
+  if ( route.name === 'index' ) {
+    return redirect('/auth/login');
+  } else {
+    return redirect(`/auth/login?${ TIMED_OUT }`);
+  }
+}
+
+/**
+ * Record in our state management that we don't have any auth providers
+ */
+export function noAuth(store) {
+  store.commit('auth/hasAuth', false);
+}
