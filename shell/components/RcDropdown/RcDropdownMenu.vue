fce8061c32abc2b29cb245bd63a76859d89ee0ca
wujun <897415845@qq.com>
2025-05-11 19:37:00 +0800

    fix: table action style

--- shell/components/RcDropdown/RcDropdownMenu.vue
+++ shell/components/RcDropdown/RcDropdownMenu.vue
@@ -1,15 +1,10 @@
 <script setup lang="ts">
-import {
-  RcDropdown,
-  RcDropdownItem,
-  RcDropdownSeparator,
-  RcDropdownTrigger
-} from '@shell/components/RcDropdown';
+import { RcDropdownSeparator } from '@shell/components/RcDropdown';
 import { RcDropdownMenuComponentProps, DropdownOption } from './types';
 import IconOrSvg from '@shell/components/IconOrSvg';
 
 // eslint-disable-next-line vue/no-setup-props-destructure
-const { buttonRole = 'primary', buttonSize = '' } = defineProps<RcDropdownMenuComponentProps>();
+const { buttonSize = '' } = defineProps<RcDropdownMenuComponentProps>();
 
 const emit = defineEmits(['update:open', 'select']);
 
@@ -19,48 +14,47 @@ const hasOptions = (options: DropdownOption[]) => {
 </script>
 
 <template>
-  <rc-dropdown
+  <a-dropdown
+    trigger="click"
+    :size="buttonSize"
     :aria-label="dropdownAriaLabel"
     @update:open="(e: boolean) => emit('update:open', e)"
   >
-    <rc-dropdown-trigger
-      :[buttonRole]="true"
-      :[buttonSize]="true"
-      :data-testid="dataTestid"
-      :aria-label="buttonAriaLabel"
-    >
+    <a-button type="link">
       <i class="icon icon-actions" />
-    </rc-dropdown-trigger>
-    <template #dropdownCollection>
-      <template
-        v-for="(a) in options"
-        :key="a.label"
-      >
-        <rc-dropdown-item
-          v-if="!a.divider"
-          @click="(e: MouseEvent) => emit('select', e, a)"
+    </a-button>
+    <template #overlay>
+      <a-menu>
+        <template
+          v-for="(a) in options"
+          :key="a.label"
         >
-          <template #before>
-            <IconOrSvg
-              v-if="a.icon || a.svg"
-              :icon="a.icon"
-              :src="a.svg"
-              class="icon"
-              color="header"
-            />
-          </template>
-          {{ a.label }}
-        </rc-dropdown-item>
-        <rc-dropdown-separator
-          v-else
-        />
-      </template>
-      <rc-dropdown-item
-        v-if="!hasOptions(options)"
-        disabled
-      >
-        No actions available
-      </rc-dropdown-item>
+          <a-menu-item
+            v-if="!a.divider"
+            @click="(e: MouseEvent) => emit('select', e, a)"
+          >
+            <template #icon>
+              <IconOrSvg
+                v-if="a.icon || a.svg"
+                :icon="a.icon"
+                :src="a.svg"
+                class="icon"
+                color="header"
+              />
+            </template>
+            {{ a.label }}
+          </a-menu-item>
+          <rc-dropdown-separator
+            v-else
+          />
+        </template>
+        <a-menu-item
+          v-if="!hasOptions(options)"
+          disabled
+        >
+          No actions available
+        </a-menu-item>
+      </a-menu>
     </template>
-  </rc-dropdown>
+  </a-dropdown>
 </template>
