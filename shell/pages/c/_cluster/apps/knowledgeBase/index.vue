d5d6e0dbc77385b4f7f860bea60d82a458b5c401
qingyang <n313893254@gmail.com>
2025-05-21 11:50:04 +0800

    feat(knowledgeBase): add create page and integrate with Weaviate API
    Add a new create page for the knowledge base feature, enabling users to create new entries. Integrate with Weaviate API to fetch and display schema data. Also, add a console log for debugging purposes in the ResourceList component.

--- shell/pages/c/_cluster/apps/knowledgeBase/index.vue
+++ shell/pages/c/_cluster/apps/knowledgeBase/index.vue
@@ -1,12 +1,71 @@
 <script>
+import { getAllSchemaAPI } from '@/shell/config/weaviate';
+import ResourceTable from '@shell/components/ResourceTable';
+import ButtonLink from '@shell/components/ButtonLink';
+
 export default {
   layout: 'plain',
+
+  components: { 
+    ResourceTable,
+    ButtonLink,
+  },
+
   data() {
-    return {};
-  }
+    return {
+      classes: [],
+      schema: {
+        
+      },
+      headers: [
+        
+      ],
+    };
+  },
+
+  async fetch() {
+    const inStore = this.$store.getters['currentProduct'].inStore;
+    const res = await this.$store.dispatch(
+      `${ inStore }/request`,
+      { url: getAllSchemaAPI }
+    );
+
+    this.classes = res.classes || [];
+  },
+
+  computed: {
+    createLocation() {
+      return {
+        name:   `c-cluster-apps-knowledgeBase-create`,
+        params: { cluster: 'local' }
+      };
+    },
+  },
 };
 </script>
 
 <template>
-  <h1>KnowledgeBase</h1>
+  <div>
+    <header>
+      <div class="title">
+        <h1 class="m-0">
+          Knowledge Base
+        </h1>
+      </div>
+      <div class="actions-container">
+        <div class="actions">
+          <button-link
+            :to="createLocation"
+          >
+            Create
+          </button-link>
+        </div>
+      </div>
+    </header>
+    <ResourceTable
+      :schema="schema"
+      :headers="headers"
+      :rows="classes"
+    />
+  </div>
 </template>
