2d22bdb6d50dfcdfed3b25a7a6497beb4e1ed162
qingyang <n313893254@gmail.com>
2025-06-08 03:17:35 +0800

    feat(knowledgeBase): add hit test functionality for documents
    - Add new hit test action to knowledge base documents
    - Create hit test page with input and results display
    - Implement similarity threshold configuration
    - Add translations for hit test feature

--- shell/models/app.llmos.ai.knowledgebase.js
+++ shell/models/app.llmos.ai.knowledgebase.js
@@ -1,5 +1,5 @@
 import SteveModel from '@shell/plugins/steve/steve-class';
-import { LLMOS } from '@shell/config/types';
+import { LLMOS, APP } from '@shell/config/types';
 import { set } from '@shell/utils/object';
 
 export default class Dataset extends SteveModel {
@@ -23,5 +23,30 @@ export default class Dataset extends SteveModel {
 
   get documentCount() {
     return (this.spec?.files || []).length;
+  }
+
+  get _availableActions() {
+    const out = super._availableActions;
+
+    out.unshift({
+      action:  'hitTest',
+      enabled: true,
+      icon:    'icon icon-fw icon-checkmark',
+      label:   this.t('knowledgeBase.actions.hitTest'),
+    });
+
+    return out;
+  }
+
+  hitTest() {
+    this.currentRouter().push({
+      name:   'c-cluster-product-resource-namespace-id-hit',
+      params: {
+        cluster: 'local',
+        resource: APP.KNOWLEDGE_BASE,
+        namespace: this.metadata.namespace,
+        id: this.metadata.name,
+      },
+    });
   }
 }
