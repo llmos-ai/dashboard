f3ce36b0b4d83e41d6a82fe711305d646e499ff9
wujun <897415845@qq.com>
2025-08-04 16:22:21 +0800

    refactor(auth): 简化设置页面逻辑并移除未使用的组件
    - 移除未使用的组件导入和模板代码
    - 简化密码修改和首次登录逻辑
    - 移除多集群管理相关代码
    - 优化表单验证和提交处理

--- shell/pages/auth/setup.vue
+++ shell/pages/auth/setup.vue
@@ -14,18 +14,15 @@ import { SETTING } from '@shell/config/settings';
 import { exceptionToErrorsArray } from '@shell/utils/error';
 import Password from '@shell/components/form/Password';
 import { applyProducts } from '@shell/store/type-map';
-import BrandImage from '@shell/components/BrandImage';
 import { waitFor } from '@shell/utils/async';
-import { Banner } from '@shell/components/Banner';
 import FormValidation from '@shell/mixins/form-validation';
 import isUrl from 'is-url';
 import { isLocalhost } from '@shell/utils/validators/setting';
-import Loading from '@shell/components/Loading';
 
 const calcIsFirstLogin = (store) => {
   const firstLoginSetting = store.getters['management/byId'](MANAGEMENT.SETTING, SETTING.FIRST_LOGIN);
 
-  return firstLoginSetting?.value === 'true';
+  return firstLoginSetting?.value === 'true' || firstLoginSetting?.value === undefined;
 };
 
 const calcMustChangePassword = async(store) => {
@@ -58,7 +55,7 @@ export default {
       haveCurrent:        false,
       username:           null,
       isFirstLogin:       false,
-      mustChangePassword: false,
+      mustChangePassword: true,
       current:            null,
       password:           randomStr(),
       confirm:            null,
@@ -71,31 +68,28 @@ export default {
     };
   },
 
-  async beforeCreate() {
-    const isFirstLogin = calcIsFirstLogin(this.$store);
-    const mustChangePassword = await calcMustChangePassword(this.$store);
+  // async beforeCreate() {
+  //   const isFirstLogin = calcIsFirstLogin(this.$store);
+  //   const mustChangePassword = true;
 
-    if (isFirstLogin) {
-      // Always show setup if this is the first log in
-      return;
-    } else if (mustChangePassword) {
-      // If the password needs changing and this isn't the first log in ensure we have the password
-      if (!!this.$store.getters['auth/initialPass']) {
-        // Got it... show setup
-        return;
-      }
-      // Haven't got it... redirect to log in so we get it
-      await this.$store.dispatch('auth/logout', null, { root: true });
+  //   if (isFirstLogin) {
+  //     // Always show setup if this is the first log in
+  //     return;
+  //   } else if (mustChangePassword) {
+  //     // If the password needs changing and this isn't the first log in ensure we have the password
+  //     if (!!this.$store.getters['auth/initialPass']) {
+  //       // Got it... show setup
+  //       return;
+  //     }
+  //     // Haven't got it... redirect to log in so we get it
+  //     await this.$store.dispatch('auth/logout', null, { root: true });
 
-      return this.$router.replace(`/auth/login?${ LOGGED_OUT }`);
-    }
-
-    // For all other cases we don't need to show setup
-    return this.$router.replace('/');
-  },
+  //     return this.$router.replace(`/auth/login?${ LOGGED_OUT }`);
+  //   }
+  // },
 
   components: {
-    AsyncButton, LabeledInput, CopyToClipboard, Checkbox, RadioGroup, Password, BrandImage, Banner, Loading
+    AsyncButton, LabeledInput, CopyToClipboard, Checkbox, RadioGroup, Password
   },
 
   async fetch() {
@@ -127,11 +121,11 @@ export default {
     const current = this.$route.query[SETUP] || this.$store.getters['auth/initialPass'];
     const v3User = this.$store.getters['auth/v3User'] ?? {};
 
-    const mcmFeature = await this.$store.dispatch('management/find', {
-      type: MANAGEMENT.FEATURE, id: 'multi-cluster-management', opt: { url: `/v1/${ MANAGEMENT.FEATURE }/multi-cluster-management` }
-    });
+    // const mcmFeature = await this.$store.dispatch('management/find', {
+    //   type: MANAGEMENT.FEATURE, id: 'multi-cluster-management', opt: { url: `/v1/${ MANAGEMENT.FEATURE }/multi-cluster-management` }
+    // });
 
-    const mcmEnabled = (mcmFeature?.spec?.value || mcmFeature?.status?.default) && productName !== 'Harvester';
+    // const mcmEnabled = (mcmFeature?.spec?.value || mcmFeature?.status?.default) && productName !== 'Harvester';
 
     let serverUrl;
 
@@ -142,17 +136,18 @@ export default {
     }
 
     const isFirstLogin = await calcIsFirstLogin(this.$store);
-    const mustChangePassword = await calcMustChangePassword(this.$store);
+
+    // const mustChangePassword = await calcMustChangePassword(this.$store);
 
     this['productName'] = productName;
     this['haveCurrent'] = !!current;
     this['username'] = me?.loginName || 'admin';
     this['isFirstLogin'] = isFirstLogin;
-    this['mustChangePassword'] = mustChangePassword;
+    this['mustChangePassword'] = true;
     this['current'] = current;
     this['v3User'] = v3User;
     this['serverUrl'] = serverUrl;
-    this['mcmEnabled'] = mcmEnabled;
+    // this['mcmEnabled'] = mcmEnabled;
     this['principals'] = principals;
   },
 
@@ -162,17 +157,17 @@ export default {
         return false;
       }
 
-      if ( this.mustChangePassword ) {
-        if ( !this.current ) {
-          return false;
-        }
+      // if ( this.mustChangePassword ) {
+      //   if ( !this.current ) {
+      //     return false;
+      //   }
 
-        if ( !this.useRandom ) {
-          if ( !this.password || this.password !== this.confirm ) {
-            return false;
-          }
-        }
-      }
+      //   if ( !this.useRandom ) {
+      //     if ( !this.password || this.password !== this.confirm ) {
+      //       return false;
+      //     }
+      //   }
+      // }
 
       if (!isUrl(this.serverUrl) || this.fvGetPathErrors(['serverUrl']).length > 0) {
         return false;
@@ -213,7 +208,8 @@ export default {
         await applyProducts(this.$store, this.$plugin);
         await this.$store.dispatch('loadManagement');
 
-        if ( this.mustChangePassword ) {
+        // update user password first if changed
+        if ( this.mustChangePassword && this.password === this.confirm) {
           const users = await this.$store.dispatch('management/findAll', {
             type: MANAGEMENT.USER,
             opt:  { url: `/v1/${ MANAGEMENT.USER }?me=true` }
@@ -222,29 +218,22 @@ export default {
 
           user.spec.password = this.password;
           await user.save();
-        } else {
-          promises.push(setSetting(this.$store, SETTING.FIRST_LOGIN, 'false'));
         }
 
-        const user = this.v3User;
-
-        user.mustChangePassword = false;
-        this.$store.dispatch('auth/gotUser', user);
+        promises.push(setSetting(this.$store, SETTING.FIRST_LOGIN, 'false'));
 
         if (this.isFirstLogin) {
-          promises.push( setSetting(this.$store, SETTING.EULA_AGREED, (new Date()).toISOString()) );
-
-          if ( this.mcmEnabled && this.serverUrl ) {
+          if ( this.serverUrl ) {
             promises.push( setSetting(this.$store, SETTING.SERVER_URL, this.serverUrl) );
           }
         }
 
         await Promise.all(promises);
 
-        await waitFor(() => !calcIsFirstLogin(this.$store), 'first login to be completed', 10000, 1000, true);
-
-        buttonCb(true);
-        this.done();
+        setTimeout(() => {
+          buttonCb(true);
+          this.done();
+        }, 2000);
       } catch (err) {
         console.error(err) ; // eslint-disable-line no-console
         buttonCb(false);
@@ -264,259 +253,154 @@ export default {
 </script>
 
 <template>
-  <Loading
-    v-if="$fetchState.pending"
-    mode="relative"
-  />
-  <form
-    v-else
-    class="setup"
-    @submit.prevent
-  >
+  <form class="setup">
     <div class="row">
-      <div class="col span-6 form-col">
-        <div>
-          &nbsp;
-        </div>
-        <div>
-          <h1 class="text-center">
-            {{ t('setup.welcome', {product}) }}
-          </h1>
+      <div>
+        <h1 class="text-center">
+          {{ t('setup.welcome', {product}) }}
+        </h1>
 
-          <template v-if="mustChangePassword">
-            <p
-              v-clean-html="t(isFirstLogin ? 'setup.setPassword' : 'setup.newUserSetPassword', { username }, true)"
-              class="text-center mb-20 mt-20 setup-title"
-            />
-            <Password
-              v-if="!haveCurrent"
-              v-model:value.trim="current"
-              autocomplete="current-password"
-              type="password"
-              :label="t('setup.currentPassword')"
-              class="mb-20"
-              :required="true"
-            />
-
-            <!-- For password managers... -->
-            <input
-              type="hidden"
-              name="username"
-              autocomplete="username"
-              :value="username"
-            >
-            <div class="mb-20">
-              <RadioGroup
-                v-model:value="useRandom"
-                data-testid="setup-password-mode"
-                name="password-mode"
-                :options="passwordOptions"
-              />
-            </div>
-            <div class="mb-20">
-              <LabeledInput
-                v-if="useRandom"
-                ref="password"
-                v-model:value.trim="password"
-                :type="useRandom ? 'text' : 'password'"
-                :disabled="useRandom"
-                data-testid="setup-password-random"
-                label-key="setup.newPassword"
-              >
-                <template
-                  v-if="useRandom"
-                  #suffix
-                >
-                  <div
-                    class="addon"
-                    style="padding: 0 0 0 12px;"
-                  >
-                    <CopyToClipboard
-                      :text="password"
-                      class="btn-sm"
-                    />
-                  </div>
-                </template>
-              </LabeledInput>
-              <Password
-                v-else
-                ref="password"
-                v-model:value.trim="password"
-                :label="t('setup.newPassword')"
-                data-testid="setup-password"
-                :required="true"
-              />
-            </div>
-            <Password
-              v-show="!useRandom"
-              v-model:value.trim="confirm"
-              autocomplete="new-password"
-              data-testid="setup-password-confirm"
-              :label="t('setup.confirmPassword')"
-              :required="true"
-            />
-          </template>
-
-          <template v-if="isFirstLogin">
-            <template v-if="mcmEnabled">
-              <hr
-                v-if="mustChangePassword"
-                class="mt-20 mb-20"
-              >
-              <p>
-                <t
-                  k="setup.serverUrl.tip"
-                  :raw="true"
-                />
-              </p>
-              <div class="mt-20">
-                <Banner
-                  v-if="showLocalhostWarning"
-                  color="warning"
-                  :label="t('validation.setting.serverUrl.localhost')"
-                  data-testid="setup-serverurl-localhost-warning"
-                />
-                <Banner
-                  v-for="(err, i) in fvGetPathErrors(['serverUrl'])"
-                  :key="i"
-                  color="error"
-                  :label="err"
-                  data-testid="setup-error-banner"
-                />
-                <LabeledInput
-                  v-model:value="serverUrl"
-                  :label="t('setup.serverUrl.label')"
-                  data-testid="setup-server-url"
-                  :rules="fvGetAndReportPathRules('serverUrl')"
-                  :required="true"
-                  @update:value="onServerUrlChange"
-                />
-              </div>
-            </template>
-
-            <div class="checkbox pt-10 eula">
-              <Checkbox
-                id="checkbox-eula"
-                v-model:value="eula"
-                data-testid="setup-agreement"
-              >
-                <template #label>
-                  <t
-                    k="setup.eula"
-                    :raw="true"
-                    :name="productName"
-                  />
-                </template>
-              </Checkbox>
-            </div>
-          </template>
-
-          <div
-            id="submit"
-            class="text-center mt-20"
+        <template v-if="mustChangePassword">
+          <p
+            v-clean-html="t(isFirstLogin ? 'setup.setPassword' : 'setup.newUserSetPassword', { username }, true)"
+            class="text-center mb-20 mt-20 setup-title"
+          />
+          <!-- For password managers... -->
+          <input
+            type="hidden"
+            name="username"
+            autocomplete="username"
+            :value="username"
           >
-            <AsyncButton
-              key="passwordSubmit"
-              type="submit"
-              mode="continue"
-              :disabled="!saveEnabled"
-              data-testid="setup-submit"
-              @click="save"
+          <div class="mb-20">
+            <RadioGroup
+              v-model:value="useRandom"
+              data-testid="setup-password-mode"
+              name="password-mode"
+              :options="passwordOptions"
+            />
+          </div>
+          <div class="mb-20">
+            <LabeledInput
+              v-if="useRandom"
+              ref="password"
+              v-model:value.trim="password"
+              :type="useRandom ? 'text' : 'password'"
+              :disabled="useRandom"
+              data-testid="setup-password-random"
+              label-key="setup.newPassword"
+            >
+              <template
+                v-if="useRandom"
+                #suffix
+              >
+                <div
+                  class="addon"
+                  style="padding: 0 0 0 12px;"
+                >
+                  <CopyToClipboard
+                    :text="password"
+                    class="btn-sm"
+                  />
+                </div>
+              </template>
+            </LabeledInput>
+            <Password
+              v-else
+              ref="password"
+              v-model:value.trim="password"
+              :label="t('setup.newPassword')"
+              data-testid="setup-password"
+              :required="true"
+            />
+          </div>
+          <Password
+            v-show="!useRandom"
+            v-model:value.trim="confirm"
+            autocomplete="new-password"
+            data-testid="setup-password-confirm"
+            :label="t('setup.confirmPassword')"
+            :required="true"
+          />
+        </template>
+
+        <br>
+        <template v-if="isFirstLogin">
+          <p>
+            <t
+              k="setup.serverUrl.tip"
+              :raw="true"
+            />
+          </p>
+          <div class="mt-20">
+            <LabeledInput
+              v-model:value="serverUrl"
+              :label="t('setup.serverUrl.label')"
+              data-testid="setup-server-url"
             />
           </div>
 
-          <div class="setup-errors mt-20">
-            <h4
-              v-for="(err, i) in errors"
-              :key="i"
-              class="text-error text-center"
+          <div class="checkbox pt-10 eula">
+            <Checkbox
+              id="checkbox-eula"
+              v-model:value="eula"
+              data-testid="setup-agreement"
             >
-              {{ err }}
-            </h4>
+              <template #label>
+                <t
+                  k="setup.eula"
+                  :raw="true"
+                  :name="productName"
+                />
+              </template>
+            </Checkbox>
           </div>
+        </template>
+        <div
+          id="submit"
+          class="text-center mt-20"
+        >
+          <AsyncButton
+            key="passwordSubmit"
+            type="primary"
+            mode="continue"
+            :disabled="!saveEnabled"
+            data-testid="setup-submit"
+            @click="save"
+          />
         </div>
-        <div>
-          &nbsp;
+
+        <div class="setup-errors mt-20">
+          <h4
+            v-for="err in errors"
+            :key="err"
+            class="text-error text-center"
+          >
+            {{ err }}
+          </h4>
         </div>
       </div>
-      <BrandImage
-        class="col span-6 landscape"
-        file-name="login-landscape.svg"
-      />
     </div>
   </form>
 </template>
 
 <style lang="scss" scoped>
-  .principal {
-    display: block;
-    background: var(--box-bg);
-    border: 1px solid var(--border);
-    border-radius: 3px;
-    margin: 10px 0;
-    padding: 10px;
-    line-height: 40px;
+.setup {
+  overflow: hidden;
+  margin: auto;
+  max-width: 500px;
 
-    img {
-      vertical-align: middle;
-      margin: 0 10px;
+  .row {
+    & .checkbox {
+      margin: auto
     }
   }
-
-  .setup {
-    overflow: hidden;
-
-    .row {
-      & .checkbox {
-        margin: auto
-      }
-
-      .span-6 {
-        padding: 0 60px;
-        margin: 0;
-      }
-
-      .landscape {
-        height: 100vh;
-        margin: 0;
-        object-fit: cover;
-        padding: 0;
-        width: 49%;
-      }
-    }
-
-    .form-col {
-      display: flex;
-      flex-direction: column;
-      overflow-y: auto;
-      position: relative;
-      height: 100vh;
-      width: 51%;
-
-      & > div:first-of-type {
-        flex: 3;
-      }
-      & > div:nth-of-type(2) {
-        flex: 9;
-      }
-      & > div:nth-of-type(3) {
-        flex: 2;
-      }
-    }
-
-    .setup-title {
-      :deep() code {
-        font-size: 12px;
-        padding: 0;
-      }
-    }
-
-    .setup-errors {
-      min-height: 50px;
-    }
-
-    p {
-      line-height: 20px;
-    }
+  .setup-errors {
+    min-height: 50px;
   }
+
+  p {
+    line-height: 20px;
+  }
+}
 </style>
